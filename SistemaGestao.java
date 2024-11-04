346adcc3f2f61e2b61ed520685582cc21416195f
kauafardin <kauafardin2004@gmail.com>
2024-11-03 23:31:51 -0300

    Classe Main

---
+++ SistemaGestao.java
@@ -0,0 +1,10 @@
+public class SistemaGestao {
+    public static void main(String[] args) {
+        
+
+        // Inicializando o gerenciamento de frota
+        GerenciamentoFrota gerenciamentoFrota = new GerenciamentoFrota();
+        gerenciamentoFrota.interagirComUsuario();
+
+    }
+}
